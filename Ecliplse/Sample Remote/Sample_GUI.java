60defe37be92d753ef13b510d2cfac3cedfda1c2
Chris Hayes <cs_hayes@hotmail.com>
2016-06-20 17:52:16 +1000

    Signed-off-by: Chris Hayes <cs_hayes@hotmail.com>

--- Ecliplse/Sample Remote/Sample_GUI.java
+++ Ecliplse/Sample Remote/Sample_GUI.java
@@ -5,6 +5,8 @@ import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Sample_GUI {
 
@@ -42,17 +44,20 @@ public class Sample_GUI {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(60, 26, 97, 25);
+		JButton btnNewButton = new JButton("Power");
+		btnNewButton.setToolTipText("Press to turn Power on or off");
+		btnNewButton.setBounds(83, 26, 97, 25);
 		frame.getContentPane().add(btnNewButton);
 		
 		JButton btnAuthenticateButton_1 = new JButton("Authenticate");
-		btnAuthenticateButton_1.setBounds(60, 64, 112, 25);
+		btnAuthenticateButton_1.setToolTipText("Press to Authenticate to TV");
+		btnAuthenticateButton_1.setBounds(134, 64, 112, 25);
 		frame.getContentPane().add(btnAuthenticateButton_1);
 		
-		JButton btnNewButton_1 = new JButton("New button");
-		btnNewButton_1.setBounds(60, 102, 97, 25);
-		frame.getContentPane().add(btnNewButton_1);
+		JButton btnConnectButton = new JButton("Connect");
+		btnConnectButton.setToolTipText("Press to Connect to TV");
+		btnConnectButton.setBounds(10, 64, 112, 25);
+		frame.getContentPane().add(btnConnectButton);
 		
 		JMenuBar menuBar = new JMenuBar();
 		frame.setJMenuBar(menuBar);
@@ -70,6 +75,13 @@ public class Sample_GUI {
 		mnFileMenu.add(mntmSaveAsMenuItem);
 		
 		JMenuItem mntmExitMenuItem = new JMenuItem("Exit");
+		mntmExitMenuItem.addActionListener(new ActionListener() 
+			{
+			public void actionPerformed(ActionEvent arg0) 
+				{
+				System.exit(1);
+				}
+			});
 		mnFileMenu.add(mntmExitMenuItem);
 	}
 }
